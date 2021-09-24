88391038ac3e8e902e6801d404ddc3c777f53a1f
Olga Droganova <olga.droganova@gmail.com>
2021-09-24 19:50:20 +0300

    setting prefixes in inkPlugin

--- tailwind-preset/src/inkPlugin.ts
+++ tailwind-preset/src/inkPlugin.ts
@@ -2,34 +2,83 @@
 
 // @ts-ignore
 import plugin from 'tailwindcss/plugin'
+import { applyPrefixed } from './applyPrefixed'
 
 // @ts-ignore
-export const inkPlugin = plugin(function ({ addUtilities }) {
+export const inkPlugin = plugin(function ({ addUtilities, prefix }) {
   // NOTE: Utility level in order to override border color on base level
   addUtilities({
     '.ui-ink': {
-      '@apply border-ink text-ink dark:border-ink-dark dark:text-ink-dark': {},
+      ...applyPrefixed(
+        prefix,
+        '.border-ink',
+        '.text-ink',
+        '.dark:border-ink-dark',
+        '.dark:text-ink-dark'
+        ),
     },
     '.ui-ink-primary': {
-      '@apply border-ink-primary text-ink-primary dark:border-ink-primary-dark dark:text-ink-primary-dark': {},
+      ...applyPrefixed(
+        prefix,
+        '.border-ink-primary',
+        '.text-ink-primary',
+        '.dark:border-ink-primary-dark',
+        '.dark:text-ink-primary-dark'
+      ),
     },
     '.ui-ink-secondary': {
-      '@apply border-ink-secondary text-ink-secondary dark:border-ink-secondary-dark dark:text-ink-secondary-dark': {},
+      ...applyPrefixed(
+        prefix,
+        '.border-ink-secondary',
+        '.text-ink-secondary',
+        '.dark:border-ink-secondary-dark',
+        '.dark:text-ink-secondary-dark'
+      ),
     },
     '.ui-ink-error': {
-      '@apply border-ink-error text-ink-error dark:border-ink-error-dark dark:text-ink-error-dark': {},
+      ...applyPrefixed(
+        prefix,
+        '.border-ink-error',
+        '.text-ink-error',
+        '.dark:border-ink-error-dark',
+        '.dark:text-ink-error-dark'
+      ),
     },
     '.ui-ink-success': {
-      '@apply border-ink-success text-ink-success dark:border-ink-success-dark dark:text-ink-success-dark': {},
+      ...applyPrefixed(
+        prefix,
+        '.border-ink-success',
+        '.text-ink-success',
+        '.dark:border-ink-success-dark',
+        '.dark:text-ink-success-dark'
+      ),
     },
     '.ui-ink-warning': {
-      '@apply border-ink-warning text-ink-warning dark:border-ink-warning-dark dark:text-ink-warning-dark': {},
+      ...applyPrefixed(
+        prefix,
+        '.border-ink-warning',
+        '.text-ink-warning',
+        '.dark:border-ink-warning-dark',
+        '.dark:text-ink-warning-dark'
+      ),
     },
     '.ui-ink-info': {
-      '@apply border-ink-info text-ink-info dark:border-ink-info-dark dark:text-ink-info-dark': {},
+      ...applyPrefixed(
+        prefix,
+        '.border-ink-info',
+        '.text-ink-info',
+        '.dark:border-ink-info-dark',
+        '.dark:text-ink-info-dark'
+      ),
     },
     '.ui-ink-border': {
-      '@apply border-ink-border text-ink-border dark:border-ink-border-dark dark:text-ink-border-dark': {},
+      ...applyPrefixed(
+        prefix,
+        '.border-ink-border',
+        '.text-ink-border',
+        '.dark:border-ink-border-dark',
+        '.dark:text-ink-border-dark'
+      ),
     },
   })
 })
