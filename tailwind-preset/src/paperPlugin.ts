030e76fdc1643d7c17364b6b11d94536ad60cce4
Olga Droganova <olga.droganova@gmail.com>
2021-09-25 11:01:39 +0300

    setting prefixes in paperPlugin

--- tailwind-preset/src/paperPlugin.ts
+++ tailwind-preset/src/paperPlugin.ts
@@ -2,37 +2,38 @@
 
 // @ts-ignore
 import plugin from 'tailwindcss/plugin'
+import { applyPrefixed } from './applyPrefixed'
 
 // @ts-ignore
-export const paperPlugin = plugin(({ addUtilities }) => {
+export const paperPlugin = plugin(({ addUtilities, prefix }) => {
   addUtilities(
     {
       '.ui-paper': {
-        '@apply bg-paper dark:bg-paper-dark': {},
+        ...applyPrefixed(prefix, '.bg-paper', '.dark:bg-paper-dark'),
       },
       '.ui-paper-2': {
-        '@apply bg-paper-2 dark:bg-paper-2-dark': {},
+        ...applyPrefixed(prefix, '.bg-paper-2', '.dark:bg-paper-2-dark'),
       },
       '.ui-paper-3': {
-        '@apply bg-paper-3 dark:bg-paper-3-dark': {},
+        ...applyPrefixed(prefix, '.bg-paper-3', '.dark:bg-paper-3-dark'),
       },
       '.ui-paper-primary': {
-        '@apply bg-paper-primary dark:bg-paper-primary-dark': {},
+        ...applyPrefixed(prefix, '.bg-paper-primary', '.dark:bg-paper-primary-dark'),
       },
       '.ui-paper-secondary': {
-        '@apply bg-paper-secondary dark:bg-paper-secondary-dark': {},
+        ...applyPrefixed(prefix, '.bg-paper-secondary', '.dark:bg-paper-secondary-dark'),
       },
       '.ui-paper-error': {
-        '@apply bg-paper-error dark:bg-paper-error-dark': {},
+        ...applyPrefixed(prefix, '.bg-paper-error', '.dark:bg-paper-error-dark'),
       },
       '.ui-paper-success': {
-        '@apply bg-paper-success dark:bg-paper-success-dark': {},
+        ...applyPrefixed(prefix, '.bg-paper-success', '.dark:bg-paper-success-dark'),
       },
       '.ui-paper-warning': {
-        '@apply bg-paper-warning dark:bg-paper-warning-dark': {},
+        ...applyPrefixed(prefix, '.bg-paper-warning', '.dark:bg-paper-warning-dark'),
       },
       '.ui-paper-info': {
-        '@apply bg-paper-info dark:bg-paper-info-dark': {},
+        ...applyPrefixed(prefix, '.bg-paper-info', '.dark:bg-paper-info-dark'),
       },
     },
     { variants: ['checked', 'focus-within'] }
