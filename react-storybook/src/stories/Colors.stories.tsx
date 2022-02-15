1f14d9c3293784e4b1d6a19ba4508dcda60b8d9e
droganov <serge@droganov.ru>
2022-02-15 16:22:10 +0300

    improve color examples

--- react-storybook/src/stories/Colors.stories.tsx
+++ react-storybook/src/stories/Colors.stories.tsx
@@ -10,7 +10,7 @@ const Template: ComponentStory<() => JSX.Element> = (args) => (
       <div className="text-ink-primary border-ink-primary dark:text-ink-primary-dark dark:border-ink-primary-dark border-2 rounded-full w-12 h-12 flex items-center justify-center">
         T
       </div>
-      <div className="bg-paper-primary dark:bg-paper-primary-dark text-ink rounded-full w-12 h-12 flex items-center justify-center">
+      <div className="ui-bg-primary text-ink rounded-full w-12 h-12 flex items-center justify-center">
         T
       </div>
     </div>
@@ -18,7 +18,7 @@ const Template: ComponentStory<() => JSX.Element> = (args) => (
       <div className="text-ink-secondary border-ink-secondary dark:text-ink-secondary-dark dark:border-ink-secondary-dark border-2 rounded-full w-12 h-12 flex items-center justify-center">
         T
       </div>
-      <div className="bg-paper-secondary dark:bg-paper-secondary-dark text-ink-dark rounded-full w-12 h-12 flex items-center justify-center">
+      <div className="ui-bg-secondary text-ink-dark rounded-full w-12 h-12 flex items-center justify-center">
         T
       </div>
     </div>
@@ -26,7 +26,7 @@ const Template: ComponentStory<() => JSX.Element> = (args) => (
       <div className="text-ink-error border-ink-error dark:text-ink-error-dark dark:border-ink-error-dark border-2 rounded-full w-12 h-12 flex items-center justify-center">
         T
       </div>
-      <div className="bg-paper-error dark:bg-paper-error-dark text-ink-dark rounded-full w-12 h-12 flex items-center justify-center">
+      <div className="ui-bg-error text-ink-dark rounded-full w-12 h-12 flex items-center justify-center">
         T
       </div>
     </div>
@@ -34,7 +34,7 @@ const Template: ComponentStory<() => JSX.Element> = (args) => (
       <div className="text-ink-success border-ink-success dark:text-ink-success-dark dark:border-ink-success-dark border-2 rounded-full w-12 h-12 flex items-center justify-center">
         T
       </div>
-      <div className="bg-paper-success dark:bg-paper-success-dark text-ink-dark rounded-full w-12 h-12 flex items-center justify-center">
+      <div className="ui-bg-success text-ink-dark rounded-full w-12 h-12 flex items-center justify-center">
         T
       </div>
     </div>
@@ -42,7 +42,7 @@ const Template: ComponentStory<() => JSX.Element> = (args) => (
       <div className="text-ink-warning border-ink-warning dark:text-ink-warning-dark dark:border-ink-warning-dark border-2 rounded-full w-12 h-12 flex items-center justify-center">
         T
       </div>
-      <div className="bg-paper-warning dark:bg-paper-warning-dark text-ink-dark rounded-full w-12 h-12 flex items-center justify-center">
+      <div className="ui-bg-warning text-ink-dark rounded-full w-12 h-12 flex items-center justify-center">
         T
       </div>
     </div>
@@ -50,20 +50,24 @@ const Template: ComponentStory<() => JSX.Element> = (args) => (
       <div className="text-ink-info border-ink-info dark:text-ink-info-dark dark:border-ink-info-dark border-2 rounded-full w-12 h-12 flex items-center justify-center">
         T
       </div>
-      <div className="bg-paper-info dark:bg-paper-info-dark text-ink-dark rounded-full w-12 h-12 flex items-center justify-center">
+      <div className="ui-bg-info text-ink-dark rounded-full w-12 h-12 flex items-center justify-center">
         T
       </div>
     </div>
+
     <div className="flex flex-col gap-2">
       <div className="text-ink border-ink dark:text-ink-dark dark:border-ink-dark border-2 rounded-full w-12 h-12 flex items-center justify-center">
         T
       </div>
-      <div className="bg-paper dark:bg-paper-dark text-ink dark:text-ink-dark rounded-full w-12 h-12 flex items-center justify-center">
+      <div className="ui-bg-paper text-ink dark:text-ink-dark rounded-full w-12 h-12 flex items-center justify-center">
         T
       </div>
     </div>
     <div className="flex flex-col gap-2 justify-end">
-      <div className="bg-paper-2 text-ink rounded-full w-12 h-12 flex items-center justify-center">
+      <div className="text-ink border-ink-border dark:border-ink-border-dark dark:text-ink-dark dark:border-ink-dark border-2 rounded-full w-12 h-12 flex items-center justify-center">
+        T
+      </div>
+      <div className="ui-bg-paper-2 text-ink dark:text-ink-dark rounded-full w-12 h-12 flex items-center justify-center">
         T
       </div>
     </div>
